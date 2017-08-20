24c31f08a5718f03491e606919e4f26a6f2675f1
scde <sd888041@gmail.com>
2017-08-20 20:12:22 +0200

    Set pins to final board

--- lichtschranke.ino
+++ lichtschranke.ino
@@ -1,23 +1,23 @@
 // TODO Set accordingly
-#define SENSOR_R 12 // with the sensors at 12 o'clock it is the RIGHT one
-/*#define SENSOR_1 12 // with the sensors at 12 o'clock it is the RIGHT one*/
-#define SENSOR_2 11 // with the sensors at 12 o'clock it is the LEFT one
-/*#define SENSOR_2 11 // with the sensors at 12 o'clock it is the LEFT one*/
+#define SENSOR_R 5 // with the sensors at 12 o'clock it is the RIGHT one
+#define SENSOR_L 9 // with the sensors at 12 o'clock it is the LEFT one
 
-#define FIRST_SECTOR 5
+#define FIRST_SECTOR 0
 #define LAST_SECTOR 5
 
-enum Sector {TOP_RIGHT, RIGHT, BOTTOM_RIGHT, BOTTOM_LEFT, LEFT, TOP_LEFT};
+/*enum Sector {TOP_RIGHT, RIGHT, BOTTOM_RIGHT, BOTTOM_LEFT, LEFT, TOP_LEFT};*/
 // TODO use enmus (NOT_CALIBRATED [maybe do not do this to enable cycling thru values],
 // TOP_RIGHT, RIGHT, BOTTOM_RIGHT, BOTTOM_LEFT, LEFT, TOP_LEFT)
 // 6 pointer values (-1 [not calibrated], 0, 60, 120, 180, 240, 300) [corresponds to sectors; left of sectors]
-int curPointerPos;
+/*int curPointerPos;*/
 // 6 sector values (-1 [not calibrated], 0, 1, 2, 3, 4, 5) [corresponds to pointer position; right of pointer position]
 int curSector;
 /*// 2 sensor values (0 [sensor1], 1 [sensor2])*/
 /*int lastSensor;*/
 
+int calibrationPointer;
 bool isCalibrated;
+
 bool enterSensorR;
 bool wasAtSensorR;
 bool exitSensorR;
@@ -32,8 +32,10 @@ void setup() {
     Serial.begin(9600);
     pinMode(SENSOR_R, INPUT);
     pinMode(SENSOR_L, INPUT);
+
     isCalibrated = false;
-    curPointerPos = -1;
+    calibrationPointer = null;
+
     curSector = -1;
     enterSensorR = false;
     wasAtSensorR = false;
@@ -44,63 +46,82 @@ void setup() {
 }
 
 void loop() {
+    checkCollisions();
     calculateSector();
+    if (!isCalibrated) {
+        if (atZeroPointer) {
+            // color only sector 0 and 5
+        }
+    }
+    else {
+        startGame();
+    }
 }
 
-void calculateSector() {
-    // TODO write function
-    // 1. read collision
+void checkCollisions() {
+    // read new sensor data
     bool atSensorR = digitalRead(SENSOR_R) == LOW;
     bool atSensorL = digitalRead(SENSOR_L) == LOW;
-    // 2. check for new collisions
-    // 2.1 check for initial collision
-    // 2.2 check for repeat collision
-    // 2.3 check for last collision
+
+    // check for new/different collision reading at right sensor
     if (wasAtSensorR != atSensorR) {
+        // check if exiting right sensor
         if (wasAtSensorR) {
             exitSensorR = true;
         }
+        // check if entering right sensor
         else {
             enterSensorR = true;
         }
     }
+    // reset enter and exit values
     else {
         enterSensorR = false;
         exitSensorR = false;
     }
 
+    // check for new/different collision reading at left sensor
     if (wasAtSensorL != atSensorL) {
+        // check if exiting left sensor
         if (wasAtSensorL) {
             exitSensorL = true;
         }
+        // check if entering left sensor
         else {
             enterSensorL = true;
         }
     }
+    // reset enter and exit values
     else {
         enterSensorL = false;
         exitSensorL = false;
     }
 
-    // 3. Calibrate if neccessary
-    if (!isCalibrated) {
-        // initial calibration
-        if (atSensorR && atSensorL) {
-            curSector = 0;
-        }
-        // exit left
-        else if (exitSensorR && !atSensorR && !atSensorL) {
-            isCalibrated = true;
-            curSector = 0;
-        }
-        // exit right
-        else if (exitSensorL && !atSensorR && !atSensorL) {
-            isCalibrated = true;
-            curSector = 5;
-        }
-    }
+    // set values for next loop
+    wasAtSensorR = atSensorR;
+    wasAtSensorL = atSensorL;
+}
 
-    // 4. set pointer position (all pointers are able to 
+void calibrateSectors() {
+    // initial calibration
+    if (atSensorR && atSensorL) {
+        atZeroPointer = true;
+        curSector = 0;
+    }
+    // exit left
+    else if (exitSensorR && !atSensorR && !atSensorL) {
+        isCalibrated = true; // starts game
+        curSector = 0;
+    }
+    // exit right
+    else if (exitSensorL && !atSensorR && !atSensorL) {
+        isCalibrated = true; // starts game
+        curSector = 5;
+    }
+}
+
+void calculateSector() {
+    // 4. set pointer position (all pointers are able to trigger both sensors)
     else {
         // trigger both sensors
         if (atSensorR && atSensorL) {
@@ -123,31 +144,28 @@ void calculateSector() {
                 }
             }
         }
-        // trigger right sensor but not the left one
-        else if (atSensorR) {
-            // moving pointer to the left into the right sensor
-            if (enterSensorR) {
-            }
-            // moving pointer to the right while triggering the right sensor
-            else if (exitSensorL) {
-            }
-        }
-        // trigger left sensor but not the right one
-        else if (atSensorL) {
-            // moving pointer to the right into the left sensor
-            if (enterSensorL) {
-            }
-            // moving pointer to the left while triggering the left sensor
-            else if (exitSensorR) {
-            }
-        }
-        // trigger no sensor
-        else {
-        }
+        /*// trigger right sensor but not the left one*/
+        /*else if (atSensorR) {*/
+            /*// moving pointer to the left into the right sensor*/
+            /*if (enterSensorR) {*/
+            /*}*/
+            /*// moving pointer to the right while triggering the right sensor*/
+            /*else if (exitSensorL) {*/
+            /*}*/
+        /*}*/
+        /*// trigger left sensor but not the right one*/
+        /*else if (atSensorL) {*/
+            /*// moving pointer to the right into the left sensor*/
+            /*if (enterSensorL) {*/
+            /*}*/
+            /*// moving pointer to the left while triggering the left sensor*/
+            /*else if (exitSensorR) {*/
+            /*}*/
+        /*}*/
+        /*// trigger no sensor*/
+        /*else {*/
+        /*}*/
     }
-
-    wasAtSensorR = atSensorR;
-    wasAtSensorL = atSensorL;
 
     Serial.println(curSector);
 }
